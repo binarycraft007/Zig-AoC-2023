a45b015f20770ce8e7f6bad19887deeb8847b1a1
elliot <elliot.huang@vantiva.com>
2024-07-19 15:06:39 +0800

    add day01

--- src/day01.zig
+++ src/day01.zig
@@ -15,7 +15,156 @@ var client: util.Client = .{
     .http_client = .{ .allocator = util.gpa },
 };
 
-pub fn main() !void {}
+pub fn main() !void {
+    var input = try client.getInput();
+    defer input.deinit();
+
+    {
+        var part1 = Part1{ .data = input.buffer };
+        const answer = try part1.solve();
+        client.submitAnswer(.{
+            .part = .part1,
+            .anwser = answer,
+        }) catch |err| switch (err) {
+            error.IncorrectAnswser => |e| return e,
+            else => try testing.expectEqual(55607, answer),
+        };
+    }
+
+    {
+        var part2 = Part2{ .data = input.buffer };
+        const answer = try part2.solve();
+        client.submitAnswer(.{
+            .part = .part2,
+            .anwser = answer,
+        }) catch |err| switch (err) {
+            error.IncorrectAnswser => |e| return e,
+            else => try testing.expectEqual(55291, answer),
+        };
+    }
+}
+
+const Part1 = struct {
+    data: []const u8,
+
+    const Input = struct {
+        line: []const u8,
+    };
+
+    pub fn solve(self: *Part1) !usize {
+        var answer: usize = 0;
+        var it = tokenizeSca(u8, self.data, '\n');
+        while (it.next()) |line| {
+            answer += try self.solveSingle(.{ .line = line });
+        }
+        return answer;
+    }
+
+    pub fn solveSingle(self: *Part1, input: Input) !usize {
+        _ = self;
+        var buf: [2]u8 = undefined;
+        var index: usize = 0;
+        var last_char: u8 = 0;
+        for (input.line) |c| {
+            if (std.ascii.isDigit(c)) {
+                defer index += 1;
+                if (index == 0) buf[index] = c;
+                last_char = c;
+            }
+        }
+        buf[1] = last_char;
+        return try parseInt(usize, &buf, 10);
+    }
+};
+
+const Part2 = struct {
+    data: []const u8,
+
+    const Input = struct {
+        line: []const u8,
+    };
+
+    pub fn solve(self: *Part2) !usize {
+        var answer: usize = 0;
+        var it = tokenizeSca(u8, self.data, '\n');
+        while (it.next()) |line| {
+            answer += try self.solveSingle(.{ .line = line });
+        }
+        return answer;
+    }
+
+    const NumMap = struct { num: []const u8, raw: u8 };
+
+    const num_map: []const NumMap = &.{
+        .{ .num = "one", .raw = '1' },
+        .{ .num = "two", .raw = '2' },
+        .{ .num = "three", .raw = '3' },
+        .{ .num = "four", .raw = '4' },
+        .{ .num = "five", .raw = '5' },
+        .{ .num = "six", .raw = '6' },
+        .{ .num = "seven", .raw = '7' },
+        .{ .num = "eight", .raw = '8' },
+        .{ .num = "nine", .raw = '9' },
+    };
+
+    pub fn solveSingle(self: *Part2, input: Input) !usize {
+        _ = self;
+        var buf: [2]u8 = undefined;
+        var index: usize = 0;
+        var last_char: u8 = 0;
+        for (input.line, 0..) |c, i| {
+            if (std.ascii.isDigit(c)) {
+                defer index += 1;
+                if (index == 0) buf[index] = c;
+                last_char = c;
+            } else if (getSpelledOutDigit(input.line[i..])) |n| {
+                defer index += 1;
+                if (index == 0) buf[index] = n;
+                last_char = n;
+            }
+        }
+        buf[1] = last_char;
+        return try parseInt(usize, &buf, 10);
+    }
+
+    fn getSpelledOutDigit(data: []const u8) ?u8 {
+        inline for (num_map) |num| {
+            if (data.len >= num.num.len) {
+                if (std.mem.eql(u8, data[0..num.num.len], num.num)) {
+                    return num.raw;
+                }
+            }
+        }
+        return null;
+    }
+};
+
+test "part1" {
+    const data =
+        \\1abc2
+        \\pqr3stu8vwx
+        \\a1b2c3d4e5f
+        \\treb7uchet
+    ;
+    var part1 = Part1{ .data = data };
+    const answer = try part1.solve();
+    try testing.expectEqual(142, answer);
+}
+
+test "part2" {
+    const data =
+        \\two1nine
+        \\eightwothree
+        \\abcone2threexyz
+        \\xtwone3four
+        \\4nineeightseven2
+        \\zoneight234
+        \\7pqrstsixteen
+    ;
+    var part2 = Part2{ .data = data };
+    const answer = try part2.solve();
+    try testing.expectEqual(281, answer);
+}
 
 // Useful stdlib functions
 const tokenizeAny = std.mem.tokenizeAny;
